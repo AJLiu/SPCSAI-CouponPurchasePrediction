4c456676055c3fe491e195d365808b9e06757137
Ivon Liu <ivon.liu2011@gmail.com>
2015-07-23 14:34:15 -0700

    Read CSV somewhat works

--- main/ReadCSV.py
+++ main/ReadCSV.py
@@ -1,26 +1,35 @@
-__author__ = 'MES'
-
-
-#class ReadCSV:
-
 import csv
-import User
+import time
+from model.User import User
+
+__author__ = 'MES'
 
 print("Hello world")
 
 user_list_file = open('../data/user_list.csv', encoding="utf8")
-#user_list_file = open('test.csv', encoding="utf8")
-user_list_raw = list(csv.reader(user_list_file))
+user_list_raw = list(csv.reader(user_list_file))[1:]    # Remove the first header row
 
-user_list = [0 * len(user_list_raw)]
+user_list = [None] * len(user_list_raw)
 
 count = 0
 for row in user_list_raw:
-    if count == 10:
-        break
+    #if count == 10:
+        #break
     # do things
-    user_list[count] = User()
+    print(str(row).encode('ascii', 'ignore'))
+    reg_date = time.strptime(row[0], "%Y-%m-%d %H:%M:%S")
+
+    withdraw_time = None
+    if row[3] != "NA":
+        withdraw_time = time.strptime(row[3], "%Y-%m-%d %H:%M:%S")
+
+    user_list[count] = User(reg_date, row[1], row[2], withdraw_time, row[4], row[5])
     count += 1
+
+print("\nPrinting from User object now")
+for row in range(0, len(user_list)):
+    print(str(user_list[row]))
+
 
 
 # How to print unicode properly
--- model/User.py
+++ model/User.py
@@ -4,10 +4,13 @@ import datetime
 
 class User:
     """Class used to hold the info found in user; data is gotten from userlist.csv"""
-    def __init__(self, regDate = datetime(), sexid = '', age = 0, withdrawDate = datetime(), prefName = "", userid = ""):
+    def __init__(self, regDate=datetime.datetime.today(), sexid='', age=0, withdrawDate=datetime.datetime.today(), prefName="", userid=""):
         self.regDate = regDate
         self.sexid = sexid
         self.age = age
         self.withdrawDate = withdrawDate
         self.prefName = prefName
         self.userid = userid
+
+    def __str__(self):
+        return "[%s, %s, %s, %s, %s, %s]" % (str(self.regDate), self.sexid, self.age, str(self.withdrawDate), str(self.prefName).encode('ascii', 'ignore'), self.userid)
