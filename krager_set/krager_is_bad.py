e199188c6f8be4e4b102248f5b2e909f907e4341
Matthew Krager <metoofarmer@yahoo.com>
2015-07-30 13:52:14 -0700

    forgot to modify the mutilated detail csv

--- krager_set/krager_is_bad.py
+++ krager_set/krager_is_bad.py
@@ -22,6 +22,9 @@ krager_detail = pd.DataFrame(columns=coupon_detail.columns)
 for i in range(0, 310):
     row = coupon_detail[coupon_detail.COUPON_ID_hash == krager_list.at[i , 'COUPON_ID_hash' ]]
     krager_detail = krager_detail.append(row)
+    coupon_detail = coupon_detail[coupon_detail.COUPON_ID_hash != krager_detail.at[i, 'COUPON_ID_hash']]
     
 krager_list.to_csv("../csv/Krager_Set/Mutilated Orginal Sheets/coupon_list_krager.csv", na_rep="NA", index=False)
 krager_detail.to_csv("../csv/Krager_Set/Mutilated Orginal Sheets/coupon_detail_krager.csv", na_rep="NA", index=False)
+coupon_list.to_csv("../csv/Krager_Set/Mutilated Orginal Sheets/coupon_list_modified_training.csv", na_rep="NA", index=False)
+coupon_detail.to_csv("../csv/Krager_Set/Mutilated Orginal Sheets/coupon_detail_modified_training.csv", na_rep="NA", index=False)
